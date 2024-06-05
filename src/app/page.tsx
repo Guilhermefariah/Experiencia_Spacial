54a59dcf35c63655d884bb29c40be93f7eee98b5
guilhermefaria321@gmail.com <guilhermefaria321@gmail.com>
2024-06-05 09:52:46 -0300

    Page
    container principal do componente

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 "use client";
-import Head from "next/head";
 
 const Page: React.FC = () => {
-  
+  return (
+    <div>
+      <main className="flex flex-col items-center justify-center min-h-screen py-2">
+          <h1 className="text-5xl font-bold">Bem Vindo</h1>
+          <p className="text-2xl">Experimente as maravilhas do universo!</p>
+      </main>
+    </div>
+  )
 }
 export default Page;
