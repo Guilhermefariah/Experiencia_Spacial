416abae9fc8f1231c1e90a085f7444d490d24a3f
guilhermefaria321@gmail.com <guilhermefaria321@gmail.com>
2024-06-05 14:57:10 -0300

    Scale

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,13 @@ const Page: React.FC = () => {
   return (
     <div>
       <main className="flex flex-col items-center justify-center min-h-screen py-2">
-          <h1 className="text-5xl font-bold float-on-hover cursor-pointer">Bem-Vindo</h1>
-          <p className="text-2xl">Experimente as maravilhas do universo!</p>
+        <h1 className="text-5xl font-bold relative mx-auto float-on-hover cursor-pointer
+         after:content-[''] after:absolute after:left-0 after:w-full after:h-[4px] 
+         after:bottom-[-5px] after:bg-black after:transition-transform after:duration-1000
+         after:scale-x-0 hover:after:scale-x-150 duration-1000">
+          Bem-Vindo
+        </h1>
+        <p className="mt-3 text-2xl">Experimente as maravilhas do universo!</p>
       </main>
     </div>
   )
