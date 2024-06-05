43fc6a0fab655aee9fd2d0b3f5db32fa58dd13ab
guilhermefaria321@gmail.com <guilhermefaria321@gmail.com>
2024-06-05 16:47:28 -0300

    useEffect
    para verificar se o elemente existe no DOM

--- src/components/planet.tsx
+++ src/components/planet.tsx
@@ -1,7 +1,11 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
 const Planet: React.FC = () => {
     const elementRef = useRef<HTMLDivElement>(null);
 
+    useEffect(() => {
+        if (!elementRef.current) return;
+        
+    }, [])
 } 
 export default Planet
