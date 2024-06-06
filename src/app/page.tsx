bf86032d3e8bba17776e3fa06fe951f4585f67f1
guilhermefaria321@gmail.com <guilhermefaria321@gmail.com>
2024-06-06 01:01:27 -0300

    planet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,19 +4,18 @@ import Planet from "@/components/planet";
 
 const Page: React.FC = () => {
   return (
-    <div>
-      <main className="flex flex-col items-center justify-center min-h-screen py-2">
-        <h1 className="text-5xl font-bold relative mx-auto float-on-hover cursor-pointer
-         after:content-[''] after:absolute after:left-0 after:w-full after:h-[4px] 
-         after:bottom-[-5px] after:bg-black after:transition-transform after:duration-1000
-         after:scale-x-0 hover:after:scale-x-150 duration-1000">
+    <div className="overflow-x-hidden flex flex-col items-center justify-normal min-h-screen">
+      <main className="text-center py-2">
+        <h1 className="text-5xl font-bold cursor-pointer relative inline-block 
+                       after:content-[''] after:absolute after:left-0 after:w-full 
+                       after:h-[4px] after:bottom-[-5px] after:bg-black 
+                       after:transition-transform after:duration-1000 
+                       after:scale-x-0 hover:after:scale-x-150 duration-1000">
           Bem-Vindo
         </h1>
-        <p className="mt-3 text-2xl">Experimente as maravilhas do universo!</p>
+        <p className="text-2xl">Experimente as maravilhas do universo!</p>
       </main>
-      <div className="flex items-center justify-center mt-8">
         <Planet />
-      </div>
     </div>
   )
 }
--- src/components/planet.tsx
+++ src/components/planet.tsx
@@ -9,12 +9,13 @@ const Planet: React.FC = () => {
 
         const scene = new THREE.Scene();
         const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-        const renderer = new THREE.WebGLRenderer();
-        renderer.setSize(window.innerWidth, window.innerHeight);
+        const renderer = new THREE.WebGLRenderer({ alpha: true });
+
+        renderer.setSize(500, 500); 
         elementRef.current.appendChild(renderer.domElement);
 
         const geometry = new THREE.BoxGeometry(1, 1, 1);
-        const material = new THREE.MeshBasicMaterial({ color: 0x87cefa });
+        const material = new THREE.MeshBasicMaterial({ color: 0x888888, transparent: true, opacity: 0.2 });
         const cube = new THREE.Mesh(geometry, material);
         scene.add(cube);
 
@@ -31,9 +32,10 @@ const Planet: React.FC = () => {
 
         return () => {
             elementRef.current?.removeChild(renderer.domElement);
-        }
+        }     
     }, [])
 
     return <div ref={elementRef} />
-}
+
+} 
 export default Planet
